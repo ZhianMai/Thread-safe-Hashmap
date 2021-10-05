8bbb2ea73bed0d325239237aa4740ebb3ebe5808
Zhian Mai <zmai@ucsd.edu>
2021-10-05 01:48:45 -0700

    Add gitignore

--- src/johnston/linkedlist/MyLinkedList.java
+++ src/johnston/linkedlist/MyLinkedList.java
@@ -8,15 +8,19 @@ import java.util.List;
 public interface MyLinkedList<V> {
   public int size();
   public boolean isEmpty();
-  public MyLinkedList add(V v);
+
   public boolean contains(V v);
   public V get(int index);
-  public boolean set(V v, int index);
   public List<V> getAll();
+
+  public MyLinkedList add(V v);
+  public boolean set(V v, int index);
+
   public boolean remove(V v);
   public MyLinkedList removeAll();
+
   /**
-   * This method is for multi-testing only.
+   * This method is for multi-threading test only.
    */
   public int getNodeLength();
 }
