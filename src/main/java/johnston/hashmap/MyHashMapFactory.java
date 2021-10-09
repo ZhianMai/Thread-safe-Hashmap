6541bc84de77faa3e98d9fac921510cc86505187
Zhian Mai <zmai@ucsd.edu>
2021-10-09 16:59:37 -0700

    Modified factory class

--- src/main/java/johnston/hashmap/MyHashMapFactory.java
+++ src/main/java/johnston/hashmap/MyHashMapFactory.java
@@ -5,54 +5,54 @@ package johnston.hashmap;
  * hash map object with debug methods or not.
  */
 public class MyHashMapFactory {
-  private static int capacity;
-  private static float loadFactor;
   // Generate hash map without debug methods
-
   // Call hash map default ctor
   public static MyHashMap newMyHashMap(ThreadSafePolicy policy) {
-    return getMyHashMap(policy, false);
+    return newMyHashMapTesting(policy);
   }
 
   // Call hash map ctor with parameter
   public static MyHashMap newMyHashMap(ThreadSafePolicy policy,
                                        int capacity, float loadFactor) {
-    MyHashMapFactory.capacity = capacity;
-    MyHashMapFactory.loadFactor = loadFactor;
-    return getMyHashMap(policy, true);
-  }
-
-  private static MyHashMap getMyHashMap(ThreadSafePolicy policy, boolean hasParam) {
-    return getMyHashMapTesting(policy, hasParam);
+    return newMyHashMapTesting(policy, capacity, loadFactor);
   }
 
   // Generate hash map with debug methods
-
   // Call hash map default ctor
   public static MyHashMapTesting newMyHashMapTesting(ThreadSafePolicy policy) {
-    return getMyHashMapTesting(policy, false);
+    return getMyHashMapTestingDefault(policy);
   }
 
   // Call hash map ctor with parameter
   public static MyHashMapTesting newMyHashMapTesting(ThreadSafePolicy policy,
                                                      int capacity, float loadFactor) {
-    MyHashMapFactory.capacity = capacity;
-    MyHashMapFactory.loadFactor = loadFactor;
-    return getMyHashMapTesting(policy, true);
+    return getMyHashMapTestingWithParam(policy, capacity, loadFactor);
   }
 
   // Return hash map object based on given enum types and parameter
-  private static MyHashMapTesting getMyHashMapTesting(ThreadSafePolicy policy, boolean hasParam) {
+  private static MyHashMapTesting getMyHashMapTestingWithParam(ThreadSafePolicy policy,
+                                                               int capacity, float loadFactor) {
     switch (policy) {
       case NoSync:
-        return hasParam ? new MyHashMapBasicImpl(capacity, loadFactor) :
-            new MyHashMapBasicImpl();
+        return new MyHashMapBasicImpl(capacity, loadFactor);
       case SyncKeyword:
-        return hasParam ? new MyHashMapSyncedImpl(capacity, loadFactor) :
-            new MyHashMapSyncedImpl();
+        return new MyHashMapSyncedImpl(capacity, loadFactor);
       case ReadWriteLock:
-        return hasParam ? new MyHashMapReentrantImpl(capacity, loadFactor) :
-            new MyHashMapReentrantImpl();
+        return new MyHashMapReentrantImpl(capacity, loadFactor);
+      default:
+        return null;
+    }
+  }
+
+  // Return hash map object based on given enum, no parameter
+  private static MyHashMapTesting getMyHashMapTestingDefault(ThreadSafePolicy policy) {
+    switch (policy) {
+      case NoSync:
+        return new MyHashMapBasicImpl();
+      case SyncKeyword:
+        return new MyHashMapSyncedImpl();
+      case ReadWriteLock:
+        return new MyHashMapReentrantImpl();
       default:
         return null;
     }
